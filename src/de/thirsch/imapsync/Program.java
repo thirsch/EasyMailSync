4d2b2628d59c144f3d987e52e4055e590fb64a58
Thomas Hirsch <thomas.hirsch@xavo.com>
2013-06-21 23:14:20 +0200

    improved logging, introduced a new parameter to disable single accounts and skip accounts that are throwing exceptions.

--- src/de/thirsch/imapsync/Program.java
+++ src/de/thirsch/imapsync/Program.java
@@ -3,6 +3,7 @@ package de.thirsch.imapsync;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -11,6 +12,7 @@ import java.util.Set;
 import javax.mail.FetchProfile;
 import javax.mail.Flags;
 import javax.mail.Folder;
+import javax.mail.Header;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
@@ -18,180 +20,179 @@ import javax.mail.Store;
 
 public class Program {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Properties props = System.getProperties();
-		props.setProperty("mail.store.protocol", "imaps");
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Properties props = System.getProperties();
+        props.setProperty("mail.store.protocol", "imaps");
 
-		try {
-			Session session = Session.getDefaultInstance(props, null);
+        Session session = Session.getDefaultInstance(props, null);
 
-			Properties config = new Properties();
-			config.load(new FileInputStream("config.properties"));
+        try {
+            Properties config = new Properties();
+            config.load(new FileInputStream("config.properties"));
 
-			int count = Integer.parseInt(config.getProperty("numberOfAccounts",
-					"0"));
+            int count = Integer.parseInt(config.getProperty("numberOfAccounts", "0"));
 
-			for (int i = 0; i < count; i++) {
-				log("===================================================================");
-				log("Synchronize account " + i);
-				log("===================================================================");
+            for (int i = 0; i < count; i++) {
+                try {
+                    Boolean enabled = Boolean.parseBoolean(config.getProperty("accountEnabled" + i, "true"));
+                    if (!enabled) {
+                        log("Skip account " + i + ", because it is disabled.");
+                        continue;
+                    }
+                    String sourceProtocol = config.getProperty("sourceProtocol" + i, "imaps");
+                    String sourceHost = config.getProperty("sourceHost" + i);
+                    String sourcePort = config.getProperty("sourcePort" + i);
+                    String sourceUser = config.getProperty("sourceUser" + i);
+                    String sourcePassword = config.getProperty("sourcePassword" + i);
 
-				String sourceProtocol = config.getProperty(
-						"sourceProtocol" + i, "imaps");
-				String sourceHost = config.getProperty("sourceHost" + i);
-				String sourcePort = config.getProperty("sourcePort" + i);
-				String sourceUser = config.getProperty("sourceUser" + i);
-				String sourcePassword = config
-						.getProperty("sourcePassword" + i);
+                    log("===================================================================");
+                    log("  Synchronizing account " + sourceUser);
+                    log("===================================================================");
 
-				Store sourceStore = session.getStore(sourceProtocol);
+                    Store sourceStore = session.getStore(sourceProtocol);
 
-				if (sourcePort != null) {
-					sourceStore.connect(sourceHost,
-							Integer.parseInt(sourcePort), sourceUser,
-							sourcePassword);
-				} else {
-					sourceStore.connect(sourceHost, sourceUser, sourcePassword);
-				}
+                    if (sourcePort != null) {
+                        sourceStore.connect(sourceHost, Integer.parseInt(sourcePort), sourceUser, sourcePassword);
+                    } else {
+                        sourceStore.connect(sourceHost, sourceUser, sourcePassword);
+                    }
 
-				log("Connected source to %s", sourceStore);
+                    log("Connected source to %s", sourceStore);
 
-				String targetProtocol = config.getProperty(
-						"targetProtocol" + i, "imaps");
-				String targetHost = config.getProperty("targetHost" + i);
-				String targetPort = config.getProperty("targetPort" + i);
-				String targetUser = config.getProperty("targetUser" + i);
-				String targetPassword = config
-						.getProperty("targetPassword" + i);
+                    String targetProtocol = config.getProperty("targetProtocol" + i, "imaps");
+                    String targetHost = config.getProperty("targetHost" + i);
+                    String targetPort = config.getProperty("targetPort" + i);
+                    String targetUser = config.getProperty("targetUser" + i);
+                    String targetPassword = config.getProperty("targetPassword" + i);
 
-				Store targetStore = session.getStore(targetProtocol);
+                    Store targetStore = session.getStore(targetProtocol);
 
-				if (targetPort != null) {
-					targetStore.connect(targetHost,
-							Integer.parseInt(targetPort), targetUser,
-							targetPassword);
-				} else {
-					targetStore.connect(targetHost, targetUser, targetPassword);
-				}
+                    if (targetPort != null) {
+                        targetStore.connect(targetHost, Integer.parseInt(targetPort), targetUser, targetPassword);
+                    } else {
+                        targetStore.connect(targetHost, targetUser, targetPassword);
+                    }
 
-				log("Connected target to %s", targetStore);
-				log("===================================================================");
+                    log("Connected target to %s", targetStore);
+                    log("===================================================================");
 
-				synchronizeMails(sourceStore, sourceStore.getDefaultFolder(),
-						targetStore, targetStore.getDefaultFolder(), 0);
+                    synchronizeMails(sourceStore, sourceStore.getDefaultFolder(), targetStore, targetStore.getDefaultFolder(), 0);
 
-				targetStore.close();
-				sourceStore.close();
-			}
+                    targetStore.close();
+                    sourceStore.close();
 
-		} catch (MessagingException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			System.exit(2);
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(3);
-		}
-	}
+                } catch (MessagingException e) {
+                    e.printStackTrace();
+                }
+            }
 
-	private static void synchronizeMails(Store sourceStore,
-			Folder sourceFolder, Store targetStore, Folder targetFolder,
-			int level) throws MessagingException {
+            log("Done!");
+            log("  Bye...");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.exit(2);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(3);
+        }
+    }
 
-		log("Synchronizing folder %s.", sourceFolder.getFullName());
+    private static void synchronizeMails(Store sourceStore, Folder sourceFolder, Store targetStore, Folder targetFolder, int level) throws MessagingException {
 
-		if ((sourceFolder.getType() & Folder.HOLDS_MESSAGES) == Folder.HOLDS_MESSAGES) {
-			synchronizeMessages(sourceFolder, targetFolder, true);
-		}
+        log("Synchronizing folder %s.", sourceFolder.getFullName());
 
-		if ((sourceFolder.getType() & Folder.HOLDS_FOLDERS) == Folder.HOLDS_FOLDERS) {
-			log("Synchronizing subfolders of %s.", sourceFolder.getName());
-			// follow the folders.
-			Folder[] sourceSubfolderList = sourceFolder.list();
+        if ((sourceFolder.getType() & Folder.HOLDS_MESSAGES) == Folder.HOLDS_MESSAGES) {
+            synchronizeMessages(sourceFolder, targetFolder, true);
+        }
 
-			for (Folder source : sourceSubfolderList) {
-				Folder targetSubfolder = targetStore.getFolder(source
-						.getFullName().replace(source.getSeparator(),
-								targetFolder.getSeparator()));
-				if (!targetSubfolder.exists()) {
-					log("Create folder %s in target store.", source.getName());
-					targetSubfolder.create(Folder.HOLDS_MESSAGES);
-				}
-				synchronizeMails(sourceStore, source, targetStore,
-						targetSubfolder, level + 1);
-			}
-		}
-	}
+        if ((sourceFolder.getType() & Folder.HOLDS_FOLDERS) == Folder.HOLDS_FOLDERS) {
+            // follow the folders.
+            Folder[] sourceSubfolderList = sourceFolder.list();
 
-	private static void synchronizeMessages(Folder sourceFolder,
-			Folder targetFolder, boolean replicateOnly)
-			throws MessagingException {
-		log("Synchronizing messages in folder %s.", sourceFolder.getName());
+            for (Folder source : sourceSubfolderList) {
+                Folder targetSubfolder = targetStore.getFolder(source.getFullName().replace(source.getSeparator(), targetFolder.getSeparator()));
+                if (!targetSubfolder.exists()) {
+                    log("Creating folder %s in target store.", source.getName());
+                    targetSubfolder.create(Folder.HOLDS_MESSAGES);
+                }
+                synchronizeMails(sourceStore, source, targetStore, targetSubfolder, level + 1);
+            }
+        }
+    }
 
-		sourceFolder.open(Folder.READ_ONLY);
-		targetFolder.open(Folder.READ_WRITE);
+    private static void synchronizeMessages(Folder sourceFolder, Folder targetFolder, boolean replicateOnly) throws MessagingException {
+        sourceFolder.open(Folder.READ_ONLY);
+        targetFolder.open(Folder.READ_WRITE);
 
-		Map<String, Message> sourceMessages = getMessages(sourceFolder);
-		Map<String, Message> targetMessages = getMessages(targetFolder);
+        Map<String, Message> sourceMessages = getMessages(sourceFolder, true);
+        Map<String, Message> targetMessages = getMessages(targetFolder, false);
 
-		Set<String> itemsToRemove = targetMessages.keySet();
-		for (String messageId : sourceMessages.keySet()) {
+        Set<String> itemsToRemove = targetMessages.keySet();
+        for (String messageId : sourceMessages.keySet()) {
 
-			boolean exists = false;
-			for (String key : targetMessages.keySet()) {
-				if (key.equalsIgnoreCase(messageId)) {
-					itemsToRemove.remove(key);
-					exists = true;
-					break;
-				}
-			}
+            boolean exists = false;
+            for (String key : targetMessages.keySet()) {
+                if (key.equalsIgnoreCase(messageId)) {
+                    itemsToRemove.remove(key);
+                    exists = true;
+                    break;
+                }
+            }
 
-			if (!exists) {
-				log("Create message %s in target store.", messageId);
-				Message message = sourceMessages.get(messageId);
+            if (!exists) {
+                log("Create message %s in target store.", messageId);
+                Message message = sourceMessages.get(messageId);
 
-				targetFolder.appendMessages(new Message[] { message });
-			}
-		}
+                targetFolder.appendMessages(new Message[] {message});
+            }
+        }
 
-		if (!replicateOnly) {
-			for (String key : itemsToRemove) {
-				log("Remove message %s", key);
+        if (!replicateOnly) {
+            for (String key : itemsToRemove) {
+                log("Remove message %s", key);
 
-				Message message = targetMessages.get(key);
-				message.setFlag(Flags.Flag.DELETED, true);
-			}
-		}
+                Message message = targetMessages.get(key);
+                message.setFlag(Flags.Flag.DELETED, true);
+            }
+        }
 
-		targetFolder.close(true);
-		sourceFolder.close(true);
-	}
+        targetFolder.close(true);
+        sourceFolder.close(true);
+    }
 
-	private static Map<String, Message> getMessages(Folder sourceFolder)
-			throws MessagingException {
-		Map<String, Message> result = new HashMap<String, Message>();
+    @SuppressWarnings("unchecked")
+    private static Map<String, Message> getMessages(Folder sourceFolder, boolean logCount) throws MessagingException {
+        Map<String, Message> result = new HashMap<String, Message>();
 
-		Message[] messages = sourceFolder.getMessages();
+        Message[] messages = sourceFolder.getMessages();
 
-		FetchProfile fp = new FetchProfile();
-		fp.add(FetchProfile.Item.ENVELOPE);
-		fp.add("Message-ID");
-		sourceFolder.fetch(messages, fp);
+        FetchProfile fp = new FetchProfile();
+        fp.add(FetchProfile.Item.ENVELOPE);
+        fp.add("Message-ID");
+        sourceFolder.fetch(messages, fp);
 
-		for (Message message : messages) {
-			String msgId = message.getHeader("Message-ID")[0];
-			result.put(msgId, message);
-		}
+        if (logCount) {
+            log("Processing %d messages...", messages.length);
+        }
 
-		return result;
-	}
+        for (Message message : messages) {
+            Enumeration<Header> headers = message.getAllHeaders();
+            while (headers.hasMoreElements()) {
+                Header nextElement = headers.nextElement();
+                if ("Message-ID".equalsIgnoreCase(nextElement.getName())) {
+                    result.put(nextElement.getValue(), message);
+                    break;
+                }
+            }
+        }
 
-	private static void log(String format, Object... arguments) {
-		System.out.println(String.format(format, arguments));
-	}
+        return result;
+    }
+
+    private static void log(String format, Object... arguments) {
+        System.out.println(String.format(format, arguments));
+    }
 }
